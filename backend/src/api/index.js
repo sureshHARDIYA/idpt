730d7f135b1fb6de13831126de1e7db60fcd3106
Suresh Kumar Mukhiya <itsmeskm99@gmail.com>
2020-09-22 12:40:14 +0200

    attempt to fix the CORS issue

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,26 +1,29 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const graphqlHTTP = require('express-graphql')
-const schema = require('./schema')
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema');
 
-const fs = require('fs')
-const cors = require('cors')
-const path = require('path')
-const helmet = require('helmet')
+const fs = require('fs');
+const cors = require('cors');
+const path = require('path');
+const helmet = require('helmet');
 
-const config = require('../../config')()
-const authMiddleware = require('../auth/authMiddleware')
-const { init: databaseInit, middleware: databaseMiddleware } = require(
-  '../database/databaseInit'
-)
+const config = require('../../config')();
+const authMiddleware = require('../auth/authMiddleware');
+const {
+  init: databaseInit,
+  middleware: databaseMiddleware,
+} = require('../database/databaseInit');
 
-app.options("*", cors());
+app.options('*', cors());
 
 var allowedOrigins = [
-  "127.0.0.1",
-  "http://localhost:3000",
-  "http://localhost:3001",
+  '127.0.0.1',
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'https://idpt.herokuapp.com/',
+  '^(https?://(?:.+.)?herokuapp.com(?::d{1,5})?)$',
 ];
 
 // Enables CORS
@@ -32,67 +35,83 @@ app.use(
       if (!origin) return callback(null, true);
       if (allowedOrigins.indexOf(origin) === -1) {
         var msg =
-          "The CORSD policy for this site does not " +
-          "allow access from the specified Origin.";
+          'The CORSD policy for this site does not ' +
+          'allow access from the specified Origin.';
         return callback(new Error(msg), false);
       }
       return callback(null, true);
-    }
-  })
+    },
+  }),
 );
 
 // Enables Helmet, a set of tools to
 // increase security.
-app.use(helmet())
+app.use(helmet());
 
 // Initializes the Database
-databaseInit().catch(error => console.error(error))
+databaseInit().catch((error) => console.error(error));
 
 // Sets up the Upload endpoint, which is required to be REST
-const upload = require('./file/upload')
-upload.mapAllUploadRequests('/api', app, databaseMiddleware, authMiddleware)
+const upload = require('./file/upload');
+upload.mapAllUploadRequests(
+  '/api',
+  app,
+  databaseMiddleware,
+  authMiddleware,
+);
 
 // Sets up the Download endpoint, which is required to be REST
-const download = require('./file/download')
-app.get('/api/download', databaseMiddleware, authMiddleware, download)
+const download = require('./file/download');
+app.get(
+  '/api/download',
+  databaseMiddleware,
+  authMiddleware,
+  download,
+);
 
 // Sets up the GraphQL endpoint
 app.use(
   '/api',
   databaseMiddleware,
   authMiddleware,
-  graphqlHTTP(req => ({
+  graphqlHTTP((req) => ({
     schema,
     graphiql: config.graphiql,
     context: {
       currentUser: req.currentUser,
-      language: req.headers['accept-language'] || 'en'
+      language: req.headers['accept-language'] || 'en',
     },
-    formatErrorFn (error) {
+    formatErrorFn(error) {
       if (process.env.NODE_ENV !== 'test') {
-        console.error(error)
+        console.error(error);
       }
 
       return {
         message: error.message,
-        code: error.originalError && error.originalError.code,
+        code:
+          error.originalError && error.originalError.code,
         locations: error.locations,
-        path: error.path
-      }
-    }
-  }))
-)
+        path: error.path,
+      };
+    },
+  })),
+);
 
 // Exposes the build of the frontend
 // to the root / of the server
-const frontendDir = path.join(__dirname, '../../../frontend/build')
+const frontendDir = path.join(
+  __dirname,
+  '../../../frontend/build',
+);
 
 if (fs.existsSync(frontendDir)) {
-  app.use('/', express.static(frontendDir))
+  app.use('/', express.static(frontendDir));
 
-  app.get('*', function (request, response) {
-    response.sendFile(path.resolve(frontendDir, 'index.html'))
-  })
+  app.get('*', function(request, response) {
+    response.sendFile(
+      path.resolve(frontendDir, 'index.html'),
+    );
+  });
 }
 
-module.exports = app
+module.exports = app;
