e13125678c60f03751f3d83e7d44b9c3de54a3e3
Suresh Kumar Mukhiya <itsmeskm99@gmail.com>
2020-10-12 12:18:46 +0200

    Remove unused code

--- frontend/src/modules/assignments/form/assignmentsFormActions.js
+++ frontend/src/modules/assignments/form/assignmentsFormActions.js
@@ -1,12 +1,10 @@
-import { InitQuestions } from 'view/assignments/form/components/helper';
+import AssignmentsService from 'modules/assignments/assignmentsService'
+import Errors from 'modules/shared/error/errors'
+import Message from 'view/shared/message'
+import { getHistory } from 'modules/store'
+import { i18n } from 'i18n'
 
-import AssignmentsService from 'modules/assignments/assignmentsService';
-import Errors from 'modules/shared/error/errors';
-import Message from 'view/shared/message';
-import { getHistory } from 'modules/store';
-import { i18n } from 'i18n';
-
-const prefix = 'ASSIGNMENT_FORM';
+const prefix = 'ASSIGNMENT_FORM'
 
 const actions = {
   RESET: `${prefix}_RESET`,
@@ -19,81 +17,58 @@ const actions = {
   UPDATE_STARTED: `${prefix}_UPDATE_STARTED`,
   UPDATE_SUCCESS: `${prefix}_UPDATE_SUCCESS`,
   UPDATE_ERROR: `${prefix}_UPDATE_ERROR`,
-
   doNew: () => {
-    return { type: actions.RESET };
+    return { type: actions.RESET }
   },
-
-  doFind: (id) => async (dispatch) => {
+  doFind: id => async dispatch => {
     try {
-      dispatch({ type: actions.FIND_STARTED });
+      dispatch({ type: actions.FIND_STARTED })
 
-      const record = await AssignmentsService.find(id);
+      const record = await AssignmentsService.find(id)
 
-      dispatch({
-        type: actions.FIND_SUCCESS,
-        payload: record,
-      });
+      dispatch({ type: actions.FIND_SUCCESS, payload: record })
     } catch (error) {
-      Errors.handle(error);
+      Errors.handle(error)
 
-      dispatch({ type: actions.FIND_ERROR });
+      dispatch({ type: actions.FIND_ERROR })
 
-      getHistory().push('/assignments');
+      getHistory().push('/assignments')
     }
   },
-  addQuestion: (questionType) => {
-    let newQuestion = InitQuestions[questionType]();
-    return {
-      type: 'EDIT_SURVEY_ADD_QUESTION',
-      payload: newQuestion,
-      questionId: newQuestion._id,
-    };
-  },
-  updateSurveyHeader: (params) => {
-    return {
-      type: 'EDIT_SURVEY_UPDATE_SURVEY_HEADER',
-      payload: params,
-    };
-  },
-  doCreate: (values) => async (dispatch) => {
+  doCreate: values => async dispatch => {
     try {
-      dispatch({ type: actions.CREATE_STARTED });
+      dispatch({ type: actions.CREATE_STARTED })
 
-      await AssignmentsService.create(values);
+      await AssignmentsService.create(values)
 
-      dispatch({ type: actions.CREATE_SUCCESS });
+      dispatch({ type: actions.CREATE_SUCCESS })
 
-      Message.success(
-        i18n('entities.assignments.create.success'),
-      );
+      Message.success(i18n('entities.assignments.create.success'))
 
-      getHistory().push('/assignments');
+      getHistory().push('/assignments')
     } catch (error) {
-      Errors.handle(error);
+      Errors.handle(error)
 
-      dispatch({ type: actions.CREATE_ERROR });
+      dispatch({ type: actions.CREATE_ERROR })
     }
   },
   doUpdate: (id, values) => async (dispatch, getState) => {
     try {
-      dispatch({ type: actions.UPDATE_STARTED });
+      dispatch({ type: actions.UPDATE_STARTED })
 
-      await AssignmentsService.update(id, values);
+      await AssignmentsService.update(id, values)
 
-      dispatch({ type: actions.UPDATE_SUCCESS });
+      dispatch({ type: actions.UPDATE_SUCCESS })
 
-      Message.success(
-        i18n('entities.assignments.update.success'),
-      );
+      Message.success(i18n('entities.assignments.update.success'))
 
-      getHistory().push(`/assignments/${id}/edit`);
+      getHistory().push(`/assignments/${id}/edit`)
     } catch (error) {
-      Errors.handle(error);
+      Errors.handle(error)
 
-      dispatch({ type: actions.UPDATE_ERROR });
+      dispatch({ type: actions.UPDATE_ERROR })
     }
-  },
-};
+  }
+}
 
-export default actions;
+export default actions
