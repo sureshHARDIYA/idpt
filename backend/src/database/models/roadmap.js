9a3ae72a8497d7598260400d98d2062a11256ad4
Suresh Kumar Mukhiya <itsmeskm99@gmail.com>
2021-04-26 11:53:40 +0200

    Update roadmap.js

--- backend/src/database/models/roadmap.js
+++ backend/src/database/models/roadmap.js
@@ -31,66 +31,76 @@ RoadmapSchema.set('toJSON', { getters: true })
 
 RoadmapSchema.set('toObject', { getters: true })
 
-// RoadmapSchema.pre('save', function(next) {
-//   const states = Object.values(this.states || {});
-//   const prerequisite = Object.values(this.prerequisite || {});
-//   const state = `${this.state}`;
-//
-//   if (states.length > 0 && states.length === this.children.length) {
-//     if (
-//       this.state === 'ACTIVATE' &&
-//       this.state !== 'PROGRESS' &&
-//       states.includes('PROGRESS')
-//     ) {
-//       this.state = 'PROGRESS';
-//     } else if (
-//       this.state === 'LOCKED' &&
-//       this.state !== 'ACTIVATE' &&
-//       states.includes('ACTIVATE')
-//     ) {
-//       this.state = 'ACTIVATE';
-//     }
-//   }
-//
-//   if (
-//     this.state === 'LOCKED' &&
-//     prerequisite.length > 0 &&
-//     !prerequisite.find((item) => item !== 'COMPLETE')
-//   ) {
-//     this.state = 'ACTIVATE';
-//   } else if (this.state === 'PROGRESS' && states.length === this.children.length && !states.find((item) => item !== 'COMPLETE')) {
-//     this.state = 'COMPLETE';
-//   }
-//
-//   this.stateModified = state !== this.state;
-//   next();
-// })
+RoadmapSchema.pre('save', function (next) {
+  const states = Object.values(this.states || {})
+  const prerequisite = Object.values(this.prerequisite || {})
+  const state = `${this.state}`
+
+  if (states.length > 0 && states.length === this.children.length) {
+    if (
+      this.state === 'ACTIVATE' &&
+        this.state !== 'PROGRESS' &&
+        states.includes('PROGRESS')
+    ) {
+      this.state = 'PROGRESS'
+    } else if (
+      this.state === 'LOCKED' &&
+        this.state !== 'ACTIVATE' &&
+        states.includes('ACTIVATE')
+    ) {
+      this.state = 'ACTIVATE'
+    }
+  }
+
+  if (
+    this.state === 'LOCKED' &&
+      prerequisite.length > 0 &&
+      !prerequisite.find(item => item !== 'COMPLETE')
+  ) {
+    this.state = 'ACTIVATE'
+  } else if (
+    this.state === 'PROGRESS' &&
+      states.length === this.children.length &&
+      !states.find(item => item !== 'COMPLETE')
+  ) {
+    this.state = 'COMPLETE'
+  }
+
+  this.stateModified = state !== this.state
+  next()
+})
 RoadmapSchema.post('updateOne', async function () {
   await this.model.findOne(this.getQuery()).then(instance => instance.save())
 })
 
-// RoadmapSchema.post('save', async function() {
-//   if (this.stateModified || !this.__v) {
-//     const { next, record, children } = await this.populate('next').populate('children').execPopulate();
-//
-//     if (this.state === 'ACTIVATE' && children.length > 0 && !children.find((item) => item.state !== 'LOCKED')) {
-//       children[0].state = 'ACTIVATE';
-//       await children[0].save();
-//     }
-//
-//     await Record.updateOne(
-//       { _id: record },
-//       { $set: { [`states.${this._id}`]: this.state } }
-//     )
-//
-//     for (const item of next) {
-//       try {
-//         item.prerequisite[this._id] = this.state;
-//         await item.save();
-//       } catch (e) {
-//         console.log('RoadmapSchema Save:', item.id, e.toString());
-//       }
-//     }
-//   }
-// })
+RoadmapSchema.post('save', async function () {
+  if (this.stateModified || !this.__v) {
+    const { next, record, children } = await this
+      .populate('next')
+      .populate('children')
+      .execPopulate()
+
+    if (
+      this.state === 'ACTIVATE' &&
+        children.length > 0 &&
+        !children.find(item => item.state !== 'LOCKED')
+    ) {
+      children[0].state = 'ACTIVATE'
+      await children[0].save()
+    }
+
+    await Record.updateOne({ _id: record }, {
+      $set: { [`states.${this._id}`]: this.state }
+    })
+
+    for (const item of next) {
+      try {
+        item.prerequisite[this._id] = this.state
+        await item.save()
+      } catch (e) {
+        console.log('RoadmapSchema Save:', item.id, e.toString())
+      }
+    }
+  }
+})
 module.exports = database.model('roadmap', RoadmapSchema)
