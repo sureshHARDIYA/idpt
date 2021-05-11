8824638c6445aafce3f8052f0f2c35cadc19a36b
RogerWisnes <rogerwisnes@gmail.com>
2021-05-11 11:31:28 +0200

    Started implementation of graph

---
+++ frontend/src/modules/cased/graph/casedGraph.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import CasedListTable from "../../../view/cased/list/CasedListTable";
+
+
+
+export default class CasedGraph extends Component {
+
+    static testFunction() {
+        console.log("%c WE ARE INSIDE TEST-FUNCTION", "color:magenta;font-weight:bold")
+        console.log({CasedListTable})
+        return "WE ARE OUT OF TEST-FUNCTION";
+    }
+
+    render() {
+        return (
+            1
+        );
+    }
+
+
+}
+
