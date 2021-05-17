3ac35a54604e8c830c1c667715ad77eaf0048052
RogerWisnes <rogerwisnes@gmail.com>
2021-05-17 18:15:49 +0200

    Added Reducers, Selectors and Actions for casedGraph, and updated casedReducers to include the supplement

--- frontend/src/modules/cased/casedReducers.js
+++ frontend/src/modules/cased/casedReducers.js
@@ -1,12 +1,14 @@
 import list from 'modules/cased/list/casedListReducers';
 import form from 'modules/cased/form/casedFormReducers';
 import view from 'modules/cased/view/casedViewReducers';
+import graph from 'modules/cased/graph/casedGraphReducers';
 import destroy from 'modules/cased/destroy/casedDestroyReducers';
 import importerReducer from 'modules/cased/importer/casedImporterReducers';
 import { combineReducers } from 'redux';
 
 export default combineReducers({
   list,
+  graph,
   form,
   view,
   destroy,
