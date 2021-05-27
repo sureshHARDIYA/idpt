d9112ac94ff2d86ee9ce882996ae287c929b5af7
RogerWisnes <rogerwisnes@gmail.com>
2021-05-27 19:20:19 +0200

    Added tabs to views

--- frontend/src/view/cased/view/CasedView.js
+++ frontend/src/view/cased/view/CasedView.js
@@ -1,72 +1,86 @@
 import model from 'modules/cased/casedModel';
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Spinner from 'view/shared/Spinner';
 import ViewWrapper from 'view/shared/styles/ViewWrapper';
 import TextViewItem from 'view/shared/view/TextViewItem';
 import ImagesViewItem from 'view/shared/view/ImagesViewItem';
 import ModuleViewItem from 'view/module/view/ModuleViewItem';
+import {Tabs} from "antd";
 
-const { fields } = model;
+const {fields} = model;
 
 class CasedView extends Component {
   renderView() {
-    const { record } = this.props;
+    const {record} = this.props;
 
     return (
       <ViewWrapper>
-        <TextViewItem
-          label={fields.id.label}
-          value={fields.id.forView(record.id)}
-        />
+        <Tabs defaultActiveKey="1">
+          <Tabs.TabPane
+            key="1"
+            tab="General Information"
+          >
+            <TextViewItem
+              label={fields.id.label}
+              value={fields.id.forView(record.id)}
+            />
 
-        <TextViewItem
-          label={fields.name.label}
-          value={fields.name.forView(record.name)}
-        />
+            <TextViewItem
+              label={fields.name.label}
+              value={fields.name.forView(record.name)}
+            />
 
-        <TextViewItem
-          label={fields.description.label}
-          value={fields.description.forView(record.description)}
-        />
+            <TextViewItem
+              label={fields.description.label}
+              value={fields.description.forView(record.description)}
+            />
 
-        <TextViewItem
-          label={fields.status.label}
-          value={fields.status.forView(record.status)}
-        />
+            <TextViewItem
+              label={fields.status.label}
+              value={fields.status.forView(record.status)}
+            />
 
-        <ImagesViewItem
-          label={fields.featuredImage.label}
-          value={fields.featuredImage.forView(record.featuredImage)}
-        />
+            <ImagesViewItem
+              label={fields.featuredImage.label}
+              value={fields.featuredImage.forView(record.featuredImage)}
+            />
 
-        <ModuleViewItem
-          label={fields.modules.label}
-          value={fields.modules.forView(record.modules)}
-        />
+            <ModuleViewItem
+              label={fields.modules.label}
+              value={fields.modules.forView(record.modules)}
+            />
 
-        <TextViewItem
-          label={fields.availableFrom.label}
-          value={fields.availableFrom.forView(record.availableFrom)}
-        />
+            <TextViewItem
+              label={fields.availableFrom.label}
+              value={fields.availableFrom.forView(record.availableFrom)}
+            />
 
-        <TextViewItem
-          label={fields.createdAt.label}
-          value={fields.createdAt.forView(record.createdAt)}
-        />
+            <TextViewItem
+              label={fields.createdAt.label}
+              value={fields.createdAt.forView(record.createdAt)}
+            />
 
-        <TextViewItem
-          label={fields.updatedAt.label}
-          value={fields.updatedAt.forView(record.updatedAt)}
-        />
+            <TextViewItem
+              label={fields.updatedAt.label}
+              value={fields.updatedAt.forView(record.updatedAt)}
+            />
+          </Tabs.TabPane>
+          <Tabs.TabPane
+            key="2"
+            tab="Constraints"
+          >
+
+          </Tabs.TabPane>
+        </Tabs>
       </ViewWrapper>
     );
   }
 
   render() {
-    const { record, loading } = this.props;
+    const {record, loading} = this.props;
 
     if (loading || !record) {
-      return <Spinner />;
+      return <Spinner/>;
     }
 
     return this.renderView();
--- frontend/src/view/task/view/TaskView.js
+++ frontend/src/view/task/view/TaskView.js
@@ -1,87 +1,99 @@
 import model from 'modules/task/taskModel';
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Spinner from 'view/shared/Spinner';
 import ViewWrapper from 'view/shared/styles/ViewWrapper';
 import TextViewItem from 'view/shared/view/TextViewItem';
 import ModuleViewItem from 'view/module/view/ModuleViewItem';
+import {Tabs} from "antd";
 //import ImagesViewItem from "../../shared/view/ImagesViewItem";
 
-const { fields } = model;
+const {fields} = model;
 
 class TaskView extends Component {
   renderView() {
-    const { record } = this.props;
+    const {record} = this.props;
 
     return (
       <ViewWrapper>
-        <TextViewItem
-          label={fields.id.label}
-          value={fields.id.forView(record.id)}
-        />
+        <Tabs defaultActiveKey="1">
+          <Tabs.TabPane
+            key="1"
+            tab="General Information"
+          >
+            <TextViewItem
+              label={fields.id.label}
+              value={fields.id.forView(record.id)}
+            />
 
-        <TextViewItem
-          label={fields.name.label}
-          value={fields.name.forView(record.name)}
-        />
+            <TextViewItem
+              label={fields.name.label}
+              value={fields.name.forView(record.name)}
+            />
 
-        <TextViewItem
-          label={fields.description.label}
-          value={fields.description.forView(record.description)}
-        />
+            <TextViewItem
+              label={fields.description.label}
+              value={fields.description.forView(record.description)}
+            />
 
-        <TextViewItem
-          label={fields.status.label}
-          value={fields.status.forView(record.status)}
-        />
+            <TextViewItem
+              label={fields.status.label}
+              value={fields.status.forView(record.status)}
+            />
 
-        <TextViewItem
-          label={fields.tags.label}
-          value={fields.tags.forView(record.tags)}
-        />
+            <TextViewItem
+              label={fields.tags.label}
+              value={fields.tags.forView(record.tags)}
+            />
 
-        <TextViewItem
-          label={fields.points.label}
-          value={fields.points.forView(record.points)}
-        />
+            <TextViewItem
+              label={fields.points.label}
+              value={fields.points.forView(record.points)}
+            />
 
-        <TextViewItem
-          label={fields.completionRequired.label}
-          value={fields.completionRequired.forView(record.completionRequired)}
-        />
+            <TextViewItem
+              label={fields.complexityLevel.label}
+              value={fields.complexityLevel.forView(record.complexityLevel)}
+            />
 
-        <TextViewItem
-          label={fields.complexityLevel.label}
-          value={fields.complexityLevel.forView(record.complexityLevel)}
-        />
+            <ModuleViewItem
+              label={fields.owner.label}
+              value={fields.owner.forView(record.owner)}
+            />
 
-        <ModuleViewItem
-            label={fields.prerequisite.label}
-            value={fields.prerequisite.forView(record.prerequisite)}
-        />
+            <TextViewItem
+              label={fields.createdAt.label}
+              value={fields.createdAt.forView(record.createdAt)}
+            />
 
-        <ModuleViewItem
-          label={fields.owner.label}
-          value={fields.owner.forView(record.owner)}
-        />
+            <TextViewItem
+              label={fields.updatedAt.label}
+              value={fields.updatedAt.forView(record.updatedAt)}
+            />
+          </Tabs.TabPane>
+          <Tabs.TabPane
+            key="2"
+            tab="Constraints"
+          >
+            <TextViewItem
+              label={fields.completionRequired.label}
+              value={fields.completionRequired.forView(record.completionRequired)}
+            />
 
-        <TextViewItem
-          label={fields.createdAt.label}
-          value={fields.createdAt.forView(record.createdAt)}
-        />
-
-        <TextViewItem
-          label={fields.updatedAt.label}
-          value={fields.updatedAt.forView(record.updatedAt)}
-        />
+            <ModuleViewItem
+              label={fields.prerequisite.label}
+              value={fields.prerequisite.forView(record.prerequisite)}
+            />
+          </Tabs.TabPane>
+        </Tabs>
       </ViewWrapper>
     );
   }
 
   render() {
-    const { record, loading } = this.props;
+    const {record, loading} = this.props;
 
     if (loading || !record) {
-      return <Spinner />;
+      return <Spinner/>;
     }
 
     return this.renderView();
