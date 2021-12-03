d4ad8a222288389e18f986a021a3731af2c612c3
jonashaneborg <jon.haneborg@gmail.com>
2021-12-03 10:34:29 +0100

    Started on POST'ing Empatica data.

--- backend/src/api/empatica/analysis.js
+++ backend/src/api/empatica/analysis.js
@@ -1,20 +1,20 @@
+const EmpaticaService = require('../../services/empaticaService')
 const csv = require('csv-parser');
 const fs = require('fs');
 
 reader();
 
-function reader() {
-    const results = [];
-    await syncRead(results);
-    console.log(results);
+async function reader() {
+    const serv = new EmpaticaService(null, null);
+    console.log(await serv.findById("61a75059a6c7bb14086f05ff"));
 
-}
-
-async function syncRead(results) {
+    /* 
+    var results = [];
     fs.createReadStream('data.csv')
         .pipe(csv())
         .on('data', (data) => results.push(data))
         .on('end', () => {
-            //console.log(results);
+            console.log(crud);
         });
+    */
 }
