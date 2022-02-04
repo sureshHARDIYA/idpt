c949c67d1b7b0ac097e93123b95cbeb81e9652f9
jonashaneborg <jon.haneborg@gmail.com>
2022-02-04 11:29:20 +0100

    Started on adding graphs

---
+++ frontend/src/view/bioAnalyzed/bioGraphForm/BioGraphForm.js
@@ -0,0 +1,126 @@
+import { Button, Form } from 'antd';
+import { Formik } from 'formik';
+import { i18n } from 'i18n';
+import model from 'modules/record/recordModel';
+import React, { Component } from 'react';
+import ViewFormItem from 'view/shared/form/items/ViewFormItem';
+import Spinner from 'view/shared/Spinner';
+import FormWrapper, {
+  tailFormItemLayout,
+} from 'view/shared/styles/FormWrapper';
+import FormSchema from 'view/shared/form/formSchema';
+import TextAreaFormItem from 'view/shared/form/items/TextAreaFormItem';
+import CasedAutocompleteFormItem from 'view/cased/autocomplete/CasedAutocompleteFormItem';
+import PatientAutocompleteFormItem from 'view/patient/autocomplete/PatientAutocompleteFormItem';
+
+const { fields } = model;
+
+class BioGraphForm extends Component {
+  schema = new FormSchema(fields.id, [
+    fields.host,
+    fields.owner,
+    fields.description
+  ]);
+
+  handleSubmit = (values) => {
+    const { id, ...data } = this.schema.cast(values);
+    // TODO
+    console.log(values);
+    this.props.onSubmit(id, data);
+  };
+
+  initialValues = () => {
+    const record = this.props.record;
+    return this.schema.initialValues(record || {});
+  };
+
+  renderForm() {
+    const { saveLoading, isEditing } = this.props;
+
+    return (
+      <FormWrapper>
+        <Formik
+          initialValues={this.initialValues()}
+          validationSchema={this.schema.schema}
+          onSubmit={this.handleSubmit}
+          render={(form) => {
+            return (
+              <Form onSubmit={form.handleSubmit}>
+                {isEditing && (
+                  <ViewFormItem
+                    name={fields.id.name}
+                    label={fields.id.label}
+                  />
+                )}
+                <CasedAutocompleteFormItem
+                  name={fields.host.name}
+                  label={fields.host.label}
+                  required={fields.host.required}
+                  form={form}
+                />
+                <PatientAutocompleteFormItem
+                  name={fields.owner.name}
+                  label={fields.owner.label}
+                  required={fields.owner.required}
+                  form={form}
+                />
+                <TextAreaFormItem
+                  name={fields.description.name}
+                  label={fields.description.label}
+                  required={fields.description.required}
+                />
+                <Form.Item
+                  className="form-buttons"
+                  {...tailFormItemLayout}
+                >
+                  <Button
+                    loading={saveLoading}
+                    type="primary"
+                    onClick={form.handleSubmit}
+                    icon="save"
+                  >
+                    {i18n('common.save')}
+                  </Button>
+
+                  <Button
+                    disabled={saveLoading}
+                    onClick={form.handleReset}
+                    icon="undo"
+                  >
+                    {i18n('common.reset')}
+                  </Button>
+
+                  {this.props.onCancel ? (
+                    <Button
+                      disabled={saveLoading}
+                      onClick={() => this.props.onCancel()}
+                      icon="close"
+                    >
+                      {i18n('common.cancel')}
+                    </Button>
+                  ) : null}
+                </Form.Item>
+              </Form>
+            );
+          }}
+        />
+      </FormWrapper>
+    );
+  }
+
+  render() {
+    const { isEditing, findLoading, record } = this.props;
+
+    if (findLoading) {
+      return <Spinner />;
+    }
+
+    if (isEditing && !record) {
+      return <Spinner />;
+    }
+
+    return this.renderForm();
+  }
+}
+
+export default RecordForm;
